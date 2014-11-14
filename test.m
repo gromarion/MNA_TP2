82734e83a2a4b7fdbabbdc8ff43adddbc7cd713a
German Romarion <grromarion@hotmail.com>
2014-11-14 10:45:13 -0300

    Added one more person to test

--- test.m
+++ test.m
@@ -1,8 +1,8 @@
 function percentage = test()
 	disp('Test program is running. Please be patient, this may take a while...');
 	
-	user_names = cellstr(['Federico Romarion'; 'Federico Elli'; 'German Romarion'; 'Juana Unamuno'; 'Leonardo Rivas'; 'Matias Rivas'; 'Milagros Rivas'; 'Sonia Rivas'; 'Stella Giunta']);
-	file_paths = cellstr(['audios/samples_3/esta_mancha_no_se_quita_'; 'audios/samples_2/que_mal_que_la_estoy_pasando_']);
+	user_names = cellstr(['Facundo Alderete'; 'Federico Romarion'; 'Federico Elli'; 'German Romarion'; 'Juana Unamuno'; 'Leonardo Rivas'; 'Matias Rivas'; 'Milagros Rivas'; 'Sonia Rivas'; 'Stella Giunta']);
+	file_paths = cellstr(['audios/samples_2/que_mal_que_la_estoy_pasando_'; 'audios/samples_3/esta_mancha_no_se_quita_']);
 
 	total_files = length(user_names) * length(file_paths);
 	files_tested = 0;
@@ -18,7 +18,7 @@ function percentage = test()
 end
 
 function [identification_amount, files_tested] = recognize_speaker(user_names, audio_path, files_tested, total_files, previous_results)
-	users = cellstr(['fede_roma.wav'; 'federelli.wav'; 'german.wav'; 'Juana.wav'; 'leonardo.wav'; 'matias.wav'; 'milagros.wav'; 'sonia.wav'; 'stella.wav']);
+	users = cellstr(['facundo.wav'; 'fede_roma.wav'; 'federelli.wav'; 'german.wav'; 'Juana.wav'; 'leonardo.wav'; 'matias.wav'; 'milagros.wav'; 'sonia.wav'; 'stella.wav']);
 	identification_amount = 0;
 
 	for i = 1 : length(users)
